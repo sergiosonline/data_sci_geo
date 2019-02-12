cf5e5cc94a5f1f374fbadb2966ee437c073203e9
Angela Kang <angelakang50@gmail.com>
2019-02-12 02:54:47 -0500

    Added parameters to set date ranges Fixed invalid values for weather conditions, etc. scrape_weather_daily and scrape_weather_hourly assume dates given are valid

--- clean_toronto_weather.R
+++ clean_toronto_weather.R
@@ -31,39 +31,30 @@ scrape_weather_hourly <- function(date = NULL, base_url = NULL, suffix_url = NUL
   # Returns:
   #   A dataframe of hourly weather statistics for a specified date and station
   
-  min_date <- str_match(suffix_url, "hlyRange=(.*?)%7C")[2]
-  max_date <- str_match(suffix_url, "%7C(.*?)&dlyRange=")[2]
   station_id <- str_match(suffix_url, "StationID=(.*?)&Prov=ON")[2]
   date <- as.Date(date)
   
-  if(min_date == "" | max_date == ""){
-    return(data.frame(station_id = station_id, Date = NA, Hour = NA, 
-                      Temp = NA, Dew_Point_Temp = NA, Rel_Humid = NA,
-                      Wind_Dir = NA, Wind_Speed = NA, Visibility = NA, Pressure = NA, 
-                      Humidex = NA, Wind_Chill = NA, Weather_Cond = NA))
-  }
-  
-  else{
-    return(paste0(base_url, "hourly_data_e.html", suffix_url, "&timeframe=1", 
-                  "&Year=", year(date), "&Month=", month(date), "&Day=", day(date), "#") %>%
-             read_html() %>%
-             html_nodes(xpath = '//*[@id="dynamicDataTable"]/table') %>%
-             html_table() %>%
-             as.data.frame() %>%
-             `colnames<-` (c("Hour", "Temp", "Dew_Point_Temp", "Rel_Humid", 
-                             "Wind_Dir", "Wind_Speed", "Visibility", "Pressure", 
-                             "Humidex", "Wind_Chill", "Weather_Cond")) %>%
-             filter(!is.na(Temp)) %>%
-             mutate_at(.vars = vars(-Hour),
-                       .funs = funs(if_else(. %in% c("NA", "LegendNANA", "LegendMM", "LegendTT"), 
-                                            NA_character_, as.character(.)))) %>%
-             mutate(Date = date,
-                    Hour = substr(Hour, 1, 2) %>% as.numeric(),
-                    station_id = station_id) %>%
-             dplyr::select(station_id, Date, Hour, Temp, Dew_Point_Temp, Rel_Humid,
-                           Wind_Dir, Wind_Speed, Visibility, Pressure, Humidex, 
-                           Wind_Chill, Weather_Cond))
-  }
+  paste0(base_url, "hourly_data_e.html", suffix_url, "&timeframe=1", 
+         "&Year=", year(date), "&Month=", month(date), "&Day=", day(date), "#") %>%
+    read_html() %>%
+    html_nodes(xpath = '//*[@id="dynamicDataTable"]/table') %>%
+    html_table() %>%
+    as.data.frame() %>%
+    `colnames<-` (c("Hour", "Temp", "Dew_Point_Temp", "Rel_Humid", 
+                    "Wind_Dir", "Wind_Speed", "Visibility", "Pressure", 
+                    "Humidex", "Wind_Chill", "Weather_Cond")) %>%
+    filter(!is.na(Temp)) %>%
+    mutate_all(funs(na_if(., ""))) %>%
+    mutate_all(funs(gsub("LegendNANA", NA, 
+                         gsub("LegendMM", NA,
+                              gsub("LegendTT", NA,
+                                   gsub("NA", NA, .)))))) %>%
+    mutate(Date = date,
+           Hour = substr(Hour, 1, 2) %>% as.numeric(),
+           station_id = station_id) %>%
+    dplyr::select(station_id, Date, Hour, Temp, Dew_Point_Temp, Rel_Humid,
+                  Wind_Dir, Wind_Speed, Visibility, Pressure, Humidex, 
+                  Wind_Chill, Weather_Cond)
 }
 
 scrape_weather_daily <- function(date = NULL, base_url = NULL, suffix_url = NULL){
@@ -77,92 +68,93 @@ scrape_weather_daily <- function(date = NULL, base_url = NULL, suffix_url = NULL
   # Returns:
   #   A dataframe of daily weather statistics for the month of specified date and station
   
-  min_date <- str_match(suffix_url, "dlyRange=(.*?)%7C")[2]
-  max_date <- str_match(suffix_url, "%7C(.*?)&mlyRange=")[2]
   station_id <- str_match(suffix_url, "StationID=(.*?)&Prov=ON")[2]
   
-  if(min_date == "" | max_date == ""){
-    return(data.frame(station_id = station_id, Date = NA, Hour = NA, 
-                      Temp = NA, Dew_Point_Temp = NA, Rel_Humid = NA,
-                      Wind_Dir = NA, Wind_Speed = NA, Visibility = NA, Pressure = NA, 
-                      Humidex = NA, Wind_Chill = NA, Weather_Cond = NA))
-  }
-  
-  else{
-    return(paste0(base_url, "daily_data_e.html", suffix_url, "&timeframe=2", 
-                  "&Year=", year(date), "&Month=", month(date), "&Day=", day(date), "#") %>%
-             read_html() %>%
-             html_nodes(xpath = '//*[@id="dynamicDataTable"]/table') %>%
-             html_table() %>%
-             as.data.frame() %>%
-             head(-4) %>%
-             `colnames<-` (c("Date", "Max_Temp", "Min_Temp", "Mean_Temp", "Heat_Days", "Cool_Days",
-                             "Tot_Rain", "Tot_Snow", "Tot_Precip", "Ground_Snow", 
-                             "Dir_Max_Gust", "Spd_Max_Gust")) %>%
-             filter(Date != "DAY") %>%
-             mutate_at(.vars = vars(-Date),
-                       .funs = funs(if_else(. %in% c("NA", "LegendNANA", "LegendMM", "LegendTT"), 
-                                    NA_character_, .))) %>%
-             mutate(station_id = station_id,
-                    Date = as.Date(paste0(year(date), "-", month(date), "-", Date))) %>%
-             dplyr::select(station_id, Date, Max_Temp, Min_Temp, Mean_Temp, Heat_Days, Cool_Days,
-                           Tot_Rain, Tot_Snow, Tot_Precip, Ground_Snow, Dir_Max_Gust, Spd_Max_Gust))
-  }
+  paste0(base_url, "daily_data_e.html", suffix_url, "&timeframe=2", 
+         "&Year=", year(date), "&Month=", month(date), "&Day=", day(date), "#") %>%
+    read_html() %>%
+    html_nodes(xpath = '//*[@id="dynamicDataTable"]/table') %>%
+    html_table() %>%
+    as.data.frame() %>%
+    head(-4) %>%
+    `colnames<-` (c("Date", "Max_Temp", "Min_Temp", "Mean_Temp", "Heat_Days", "Cool_Days",
+                    "Tot_Rain", "Tot_Snow", "Tot_Precip", "Ground_Snow", 
+                    "Dir_Max_Gust", "Spd_Max_Gust")) %>%
+    filter(Date != "DAY") %>%
+    mutate_all(funs(na_if(., ""))) %>%
+    mutate_all(funs(gsub("LegendNANA", NA, 
+                         gsub("LegendMM", NA,
+                              gsub("LegendTT", NA,
+                                   gsub("NA", NA, .)))))) %>%
+    mutate(station_id = station_id,
+           Date = as.Date(paste0(year(date), "-", month(date), "-", Date))) %>%
+    dplyr::select(station_id, Date, Max_Temp, Min_Temp, Mean_Temp, Heat_Days, Cool_Days,
+                  Tot_Rain, Tot_Snow, Tot_Precip, Ground_Snow, Dir_Max_Gust, Spd_Max_Gust)
 }
 
-scrape_weather <- function(base_url = base_url, suffix_url = NULL, date_type = "hourly"){
+scrape_weather <- function(start_date = NULL, end_date = start_date,
+                           base_url = base_url, suffix_url = NULL, date_type = "hourly"){
   # Generates weather statistics found in Government of Canada's website
   #
   # Args:
+  #   start_date: starting date of desired time period (e.g "2018-01-01")
+  #   end_date: ending date of desired time period, must be after start_date; if some dates are not,
+  #             found function will return as much of the interval that's available
   #   base_url: string url of Govenment of Canada's climate data
   #   suffix_url: string of url for the particular weather station
   #   date_type: frequency of weather statistic; either "hourly" or "daily"
   #
   # Returns:
-  #   A dataframe of weather statistics for specified station and frequency and for all dates available
+  #   A dataframe of weather statistics for specified station and frequency and for dates specified
   
   if(date_type == "hourly"){
-    date_start <- str_match(suffix_url, "dlyRange=(.*?)%7C")[2]
-    date_end <- str_match(suffix_url, "%7C(.*?)&dlyRange=")[2]
+    min_date <- str_match(suffix_url, "dlyRange=(.*?)%7C")[2]
+    max_date <- str_match(suffix_url, "%7C(.*?)&dlyRange=")[2]
     
-    if(date_start == "" | date_end == ""){
+    if(min_date == "" | max_date == ""){
       return(data.frame(station_id = station_id, Date = NA, Hour = NA, 
                         Temp = NA, Dew_Point_Temp = NA, Rel_Humid = NA,
                         Wind_Dir = NA, Wind_Speed = NA, Visibility = NA, Pressure = NA, 
                         Humidex = NA, Wind_Chill = NA, Weather_Cond = NA))
     }
     else{
-      map_dfr(seq.Date(as.Date(date_start), as.Date(date_end), by = "day"), 
+      actual_start <- max(as.Date(min_date), as.Date(start_date))
+      actual_end <- min(as.Date(max_date), as.Date(end_date))
+      
+      map_dfr(seq.Date(as.Date(actual_start), as.Date(actual_end), by = "day"), 
               ~scrape_weather_hourly(.x, base_url, suffix_url)) %>%
         mutate(Frequency = "Hourly") 
     }
   }
   
-  if(date_type == "daily"){
-    date_start <- str_match(suffix_url, "dlyRange=(.*?)%7C")[2]
-    date_end <- str_match(suffix_url, "%7C(.*?)&dlyRange=")[2]
+  else if(date_type == "daily"){
+    min_date <- str_match(suffix_url, "dlyRange=(.*?)%7C")[2]
+    max_date <- str_match(suffix_url, "%7C(.*?)&dlyRange=")[2]
     
-    if(day(as.Date(date_start)) < day(as.Date(date_end))){
-      map_dfr(seq.Date(as.Date(date_start), as.Date(date_end), by = "month"),
-              ~scrape_weather_daily(.x, base_url, suffix_url)) %>%
-        mutate(Frequency = "Daily")
+    if(min_date == "" | max_date == ""){
+      return(data.frame(station_id = station_id, Date = NA, 
+                        Max_Temp = NA, Min_Temp = NA, Mean_Temp = NA, Heat_Days = NA, Cool_Days = NA,
+                        Tot_Rain = NA, Tot_Snow = NA, Tot_Precip = NA, Ground_Snow = NA, 
+                        Dir_Max_Gust = NA, Spd_Max_Gust = NA))
     }
+    
     else{
-      map_dfr(seq.Date(as.Date(date_start), 
-                       as.Date(paste0(year(as.Date(date_end)), "-", 
-                                      month(as.Date(date_end)) + 1, "-", 
-                                      day(as.Date(date_end)))), 
-                       by = "month"), 
-              ~scrape_weather_daily(.x, base_url, suffix_url)) %>%
-        mutate(Frequency = "Daily")
+      actual_start <- max(as.Date(min_date), as.Date(start_date))
+      actual_end <- min(as.Date(max_date), as.Date(end_date))
+      
+      if(day(as.Date(actual_start)) <= day(as.Date(actual_end))){
+        map_dfr(seq.Date(as.Date(actual_start), as.Date(actual_end), by = "month"),
+                ~scrape_weather_daily(.x, base_url, suffix_url)) %>%
+          mutate(Frequency = "Daily")
+      }
+      
+      else{
+        map_dfr(seq.Date(as.Date(actual_start), 
+                         as.Date(paste0(year(as.Date(actual_end)), "-", 
+                                        month(as.Date(actual_end)) + 1, "-01")), by = "month"), 
+                ~scrape_weather_daily(.x, base_url, suffix_url)) %>%
+          mutate(Frequency = "Daily")
+      }
     }
   }
 }
-
-
-# Final dataframes for both hourly and daily data for all available weather stations
-hourly_weather <- map_df(url_df$url, ~scrape_weather(base_url, .x, date_type = "hourly")) %>%
-  left_join(dplyr::select(url_df, -url), by = c("station_id" = "station_id"))
-
-daily_weather <- map_df(url_df$url, ~scrape_weather(base_url, .x, date_type = "daily")) %>%
-  left_join(dplyr::select(url_df, -url), by = c("station_id" = "station_id"))
