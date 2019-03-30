e3f7978f3630830e4a1819c691ae787332a6e4ea
Angela Kang <angelakang50@gmail.com>
2019-03-30 14:05:33 -0400

    Update Final Merging Script.R

--- data/final/Final Merging Script.R
+++ data/final/Final Merging Script.R
@@ -3,6 +3,7 @@
 # Accident level data
 accident_location <- "https://raw.githubusercontent.com/sergiosonline/data_sci_geo/master/data/final/automobile.csv"
 weather_location <- "https://raw.githubusercontent.com/sergiosonline/data_sci_geo/master/data/final/weather.csv"
+pop_proj_location <- "https://raw.githubusercontent.com/sergiosonline/data_sci_geo/master/data/toronto_hood_projections_2007-2017.csv"
 ###################################
 
 library(tidyverse)
@@ -17,8 +18,15 @@ weather_raw <- fread(weather_location) %>%
          tot_precip_mm = `Total Precip (mm)`, ground_snow_cm = `Snow on Grnd (cm)`) %>%
   select(date, max_temp, min_temp, tot_rain_mm, tot_snow_cm, tot_precip_mm, ground_snow_cm) %>%
   mutate(date = as.Date(date))
+pop_raw <- fread(pop_proj_location) %>%
+  select(HoodID, starts_with("Pop2")) %>%
+  gather(Year, Population, -HoodID) %>%
+  mutate(Year = as.numeric(gsub("Pop", "", Year)))
 
 accidents <- left_join(accidents_raw, weather_raw, by = c("date" = "date")) %>%
+  mutate(year = year(date)) %>%
+  left_join(pop_raw, by = c("hood_num" = "HoodID", "year" = "Year")) %>%
+  select(-year) %>%
   mutate_if(is.numeric, funs(ifelse(is.na(.), 0, .)))
 
-# fwrite(accidents, file = "C:/Users/angel/Downloads/accidents.csv")
+fwrite(accidents, file = "C:/Users/angel/Downloads/accidents.csv")
